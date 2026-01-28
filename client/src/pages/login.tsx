b3652af7ba5f0f3a4cba2ebb33ef241e567fd078
pampers23 <dexterpimss23@gmail.com>
2026-01-28 11:54:19 +0800

    feat: add image auth login

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -6,13 +6,6 @@ const Login = () => {
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
         <LoginForm />
-        <div className="bg-muted relative hidden md:block">
-            <img
-            src="/placeholder.svg"
-            alt="Image"
-            className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
-        </div>
       </div>
     </div>
   )
