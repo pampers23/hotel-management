dab4e4466f58acf063cd087544bdebe9990f5127
pampers23 <dexterpimss23@gmail.com>
2026-01-28 12:46:51 +0800

    feat: add forgot password page

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -71,7 +71,7 @@ export function LoginForm({
                 <div className="flex items-center">
                   <FieldLabel htmlFor="password">Password</FieldLabel>
                   <a
-                    href="#"
+                    href="/forgot-password"
                     className="ml-auto text-sm underline-offset-2 hover:underline"
                   >
                     Forgot your password?
