35611993b178a57495049fbf2d3f5630ef1626a9
pampers23 <dexterpimss23@gmail.com>
2026-02-06 11:20:07 +0800

    fix: login function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,34 @@
+import 'dotenv/config'
+
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import 'dotenv/config'
 import { errorHandlerMiddleware } from './middlewares/error-handle'
-import { routes } from './routes/routes';
-import { cors } from 'hono/cors';
+import { routes } from './routes/routes'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-// cors (allow vite dev server)
 app.use(
   '*',
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://lumie-re-hotel.vercel.app'],
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowHeaders: ['Content-Type', 'Authorization'],
   })
 )
 
-// handle error
-app.onError(errorHandlerMiddleware);
+app.onError(errorHandlerMiddleware)
 
-// routes
 routes.forEach((route) => {
-  app.route('/auth', route);
+  app.route('/auth', route)
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get('/health', (c) => c.text('ok'))
+
+serve(
+  {
+    fetch: app.fetch,
+    port: Number(process.env.PORT ?? 3000),
+  },
+  (info) => console.log(`Server running on http://localhost:${info.port}`)
+)
